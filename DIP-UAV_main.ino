bcff63bdbbf3f5cfac888cf1f8c8131c2c566f3c
Endilie Yacop Sucipto <endiliey@gmail.com>
2016-11-03 23:29:14 +0800

    Last working "autoland" code

--- DIP-UAV_main.ino
+++ DIP-UAV_main.ino
@@ -15,74 +15,24 @@
 #define FRAME_LENGTH 15000  //set the PPM frame length in microseconds (1ms = 1000µs)
 #define PULSE_LENGTH 300  //set the pulse length
 #define THROTTLE_LANDING_VALUE 900 // please change this with the value of throttle when its going to DESCEND (THE 900 VALUE IS ASSUMPTION BY ENDI)
-#define LANDING_HEIGHT_VALUE 50 // please change this with the value of object height when the drones are very near with the object (like ~20cm distant apart)
+#define LANDING_HEIGHT_VALUE 300 // please change this with the value of object height when the drones are very near with the object (like ~20cm distant apart)
 #define onState 1  //set polarity of the pulses: 1 is positive, 0 is negative
 #define sigPin 8  //set PPM signal output pin on the arduino
-#define LANDING_X_VALUE 20 // set the desired landing area (more of error-tolerance within X)
-#define LANDING_Y_VALUE 20 // set the desired landing area (Y error-tolerance)
-#define TURNING_SPEED 200 // set the desired maximum turning speed. The bigger it is, the faster the drones may turn/ move itself for autolanding
+#define LANDING_X_VALUE 2 // set the desired landing area (more of error-tolerance within X)
+#define LANDING_Y_VALUE 2 // set the desired landing area (Y error-tolerance)
+#define TURNING_SPEED 70 // set the desired maximum turning speed. The bigger it is, the faster the drones may turn/ move itself for autolanding
 #define LANDING_SPEED 25 // set the desired landing speed (make sure its around 100. Bigger numbers = faster throttle down for autolanding
 #include <SPI.h>  // include SPI interface, note that by using this, pin 10,11,12,13 cannot be used (take note for our project seriously)
 #include <Pixy.h> // include Pixy header files, this is the basic header to get Pixy method functioning
 #include <PinChangeInterrupt.h> // include hardware interrupt libraries to get PWM input without lot of delay (usually we use PulseIn which gives lot of delay)
 #define X_CENTER        ((PIXY_MAX_X-PIXY_MIN_X)/2)       
 #define Y_CENTER        ((PIXY_MAX_Y-PIXY_MIN_Y)/2)
-#define P_GAIN 200
-#define I_GAIN 150
-#define D_GAIN 50
+#include <PID_v1.h>
+#define ledPin 9
 
 Pixy pixy; // Create an instances of Pixy class named pixy
 
-/*Creating a class to store our PID tuning*/
-class PIDLoop
-{
- public:
- PIDLoop(int32_t pgain, int32_t igain, int32_t dgain);
-
- void update(int32_t error);
-
- int32_t m_pos;
- int32_t m_prevError;
- int32_t m_pgain;
- int32_t m_dgain;
- int32_t m_igain;
- int32_t m_sumError;
-};
-
-/*PIDLoop Hard-coded Constructor*/
-PIDLoop rollLoop(P_GAIN, I_GAIN, D_GAIN);
-PIDLoop pitchLoop(P_GAIN, I_GAIN, D_GAIN);
-
-/*PIDLoop Constructor*/
-PIDLoop::PIDLoop(int32_t pgain, int32_t igain, int32_t dgain)
-{
-  m_pos = CHANNEL_DEFAULT_VALUE;
-  m_pgain = pgain;
-  m_dgain = dgain;
-  m_igain = igain;
-  m_prevError = 0x80000000L;
-  m_sumError = 0;
-}
-
-/*PIDLoop Method*/
-void PIDLoop::update(int32_t error)
-{
-  long int vel;
-  
-  m_sumError += error;
-  
-  if (m_prevError!=0x80000000)
-  {  
-    vel = (error*m_pgain + m_sumError*m_igain + (error - m_prevError)*m_dgain)>>10;
-    m_pos += vel;
-    if (m_pos> CHANNEL_DEFAULT_VALUE + TURNING_SPEED) 
-      m_pos = CHANNEL_DEFAULT_VALUE + TURNING_SPEED; 
-    else if (m_pos < CHANNEL_DEFAULT_VALUE - TURNING_SPEED) 
-      m_pos = CHANNEL_DEFAULT_VALUE - TURNING_SPEED;
-  }
-  m_prevError = error;
-}
-
+ 
 /*this array is the global variable needed to get and store PWM valuee*/
 const byte channel_pin[] = {2,3,4,5,6,7}; // we use pin 2,3,4,5,6,7 for PWM input
 volatile unsigned long rising_start[] = {0,0,0,0,0,0};
@@ -96,6 +46,7 @@ bool autoLand = false;
 bool objectFound = false;
 int throttleLast;
 bool descendBefore = false ;
+
 /*To make things readable, we create an arrays to hold the yaw, throttle, pitch, roll and ch5,ch6 PWM Value */
 volatile long yaw;
 volatile long throttle;
@@ -108,8 +59,34 @@ volatile long ch6;
  change theese values in your code (usually servo values move between 1000 and 2000)*/
 int ppm[CHANNEL_NUMBER];
 
+/* New Method of using PID Library*/
+double rollInput; // Error of roll, in this case is "X", we want it to be equal to setpoint
+double pitchInput; // Error of pitch, in this case is "Y", we want it to be equal to setpoint
+double Kp = 2.5 ; // P constant
+double Ki = 6; // I constant 
+double Kd = 0.8; // D constant
+double Setpoint = 0; // desired setpoint, we want it to be exactly in the middle
+double rollOutput; // the PWM value of roll that PID keep adjusting to get Output equal to setpoint
+double pitchOutput; // the PWM value of pitch that PID keep adjusting to get Input equal to setpoint
+double rollFinal;
+double pitchFinal;
+PID rollPID(&rollInput, &rollOutput, &Setpoint, Kp, Ki, Kd, REVERSE); 
+PID pitchPID(&pitchInput, &pitchOutput, &Setpoint, Kp, Ki, Kd, REVERSE  );
+/*End of PID Library declaration*/
+
 void setup() 
-{
+{ 
+
+  /*Setup for rollPID and pitchPID*/
+  rollPID.SetMode(AUTOMATIC);
+  pitchPID.SetMode(AUTOMATIC);
+  rollPID.SetSampleTime(5); //To make sure it's fast. we use 30ms to autocorrect
+  pitchPID.SetSampleTime(5);
+  rollPID.SetOutputLimits(-1 * TURNING_SPEED, TURNING_SPEED);
+  pitchPID.SetOutputLimits(-1 * TURNING_SPEED, TURNING_SPEED);
+  //rollOutput = CHANNEL_DEFAULT_VALUE;
+  //pitchOutput = CHANNEL_DEFAULT_VALUE;
+  
     //initialize default ppm values
     for(int i=0; i<CHANNEL_NUMBER; i++)
     {
@@ -122,6 +99,8 @@ void setup()
           ppm[i]= CHANNEL_DEFAULT_VALUE;  
         }
     }
+
+    pinMode(ledPin, OUTPUT);
     
     /*this is the initializer needed for PPM Signal Generation */
     pinMode(sigPin, OUTPUT);
@@ -212,7 +191,7 @@ ISR(TIMER1_COMPA_vect)
       { 
         //start pulse
         digitalWrite(sigPin, onState);
-        OCR1A = PULSE_LENGTH * 2;
+        OCR1A =  PULSE_LENGTH * 2;
         state = false;
       } 
     else
@@ -244,7 +223,9 @@ void loop() {
 
 
   /*This store the PWM Value that we get to its respective variable (just for easier readability), rather than having channel_length[0], we use yaw instead*/
-
+  Serial.print(x);
+  Serial.print("|");
+  Serial.println(y);
   throttle = channel_length[0];
   roll = channel_length[1];
   pitch = channel_length[2];
@@ -256,10 +237,12 @@ void loop() {
          if ( pixy.getBlocks() > 0 )
          {
           objectFound = true;
+          digitalWrite(ledPin, HIGH);
          }  
          else
          {
           objectFound = false;
+          digitalWrite(ledPin, LOW);
          }  
  
          if ( ch5 <= CHANNEL_DEFAULT_VALUE)
@@ -272,6 +255,7 @@ void loop() {
               {
                 autoLand = true; 
                 throttleLast = throttle;
+                
               }
               else
               {
@@ -283,9 +267,12 @@ void loop() {
   if (objectFound == true)
   {
 
-        x = (pixy.blocks[0].x) - X_CENTER ;
-        y = -1 * ((pixy.blocks[0].y) - Y_CENTER) ;
+        x = X_CENTER - pixy.blocks[0].x  ;
+        y = (pixy.blocks[0].y) - Y_CENTER ;
         height = pixy.blocks[0].height;
+
+        rollInput = (double)x;
+        pitchInput = (double)y;
         
   /*This part is only for debugging, we will comment this out later on */
             /*Serial.println("Detected: ");
@@ -309,7 +296,6 @@ void loop() {
   Only if autoLand is not ON it will be manual mode */
    if ( autoLand == false )
    {  
-      Serial.println("MANUAL MODE");
       ppm[0] = throttle;
       ppm[1] = roll;
       ppm[2] = pitch;
@@ -319,62 +305,76 @@ void loop() {
    }
    else
    {
-      Serial.println("Autoland true and object found true");
+    ppm[0] = throttle;
+    ppm[3] = yaw;
+    Serial.println(height);
            if (height >= LANDING_HEIGHT_VALUE)
               {
-                Serial.println("LANDING perfectly");
-                ppm[0] = THROTTLE_LANDING_VALUE;
+                Serial.println("LANDING PERFECTLY, CUT THROTTLE");
+                
+                //ppm[0] = THROTTLE_LANDING_VALUE;
                 ppm[1] = CHANNEL_DEFAULT_VALUE;
                 ppm[2] = CHANNEL_DEFAULT_VALUE;
               }
            else
-           {
+              {                    
+                                /* PID Tuning to make quadcopter centered */
+                                rollPID.Compute(); // PID will adjust the roll offset PWM Value
+                                pitchPID.Compute(); // PID will adjust the pitch offset PWM Value
+                                rollFinal = CHANNEL_DEFAULT_VALUE + rollOutput;
+                                pitchFinal = CHANNEL_DEFAULT_VALUE + pitchOutput;
+
+                                    if (rollFinal > CHANNEL_DEFAULT_VALUE + TURNING_SPEED)
+                                    {
+                                      rollFinal = CHANNEL_DEFAULT_VALUE + TURNING_SPEED;
+                                    }
+                                    else if (rollFinal < CHANNEL_DEFAULT_VALUE - TURNING_SPEED)
+                                    {
+                                      rollFinal = CHANNEL_DEFAULT_VALUE - TURNING_SPEED;
+                                    }
+                                     if (pitchFinal > CHANNEL_DEFAULT_VALUE + TURNING_SPEED)
+                                    {
+                                      pitchFinal = CHANNEL_DEFAULT_VALUE + TURNING_SPEED;
+                                    }
+                                    else if (pitchFinal < CHANNEL_DEFAULT_VALUE - TURNING_SPEED)
+                                    {
+                                      pitchFinal = CHANNEL_DEFAULT_VALUE - TURNING_SPEED;
+                                    }
+                                    
+                                ppm[1] = (int)rollFinal; // Arduino output adjusted roll PWM value to flight controller
+                                ppm[2] = (int)pitchFinal; // Arduino output adjusted pitch PWM value
+                                
                             if ( x >= (-1 *LANDING_X_VALUE) && x <= LANDING_X_VALUE && y >= (-1 * LANDING_Y_VALUE) && y <= LANDING_Y_VALUE )
                             {
                                 Serial.println("Perfect, throttling down");
                                 Serial.println(height);
                                 descendBefore = true;
-                                ppm[0] = throttleLast - LANDING_SPEED;
-                                ppm[1] = CHANNEL_DEFAULT_VALUE;
-                                ppm[2] = CHANNEL_DEFAULT_VALUE;
+                                //ppm[0] = throttleLast - LANDING_SPEED;
                                 
                              }
                             else 
                             {
-                           /*if its not within area of landing, use PWMLoop to auto-correct its position */
+                                  
                                  if (descendBefore == true)
                                  {
-                                    ppm[0] = throttleLast + LANDING_SPEED;
+                                    //ppm[0] = throttleLast + LANDING_SPEED;
                                     descendBefore = false;
                                  }
                                  else
                                  {
-                                    ppm[0] = throttleLast;
+                                    //ppm[0] = throttleLast;
                                  }
-                               //Serial.print("Throttle: ");
-                               //Serial.println(ppm[0]);
-                               rollError = -1* x;
-                               pitchError = -1* y;
-                    
-                               rollLoop.update(rollError);
-                               pitchLoop.update(pitchError);
-                    
-                               ppm[1] = rollLoop.m_pos;
-                               ppm[2] = pitchLoop.m_pos;
-                               //Serial.print(x);
-                               //Serial.print("|");
-                               //Serial.println(y);
-                               //Serial.print(ppm[1]);
-                               //Serial.print("|");
-                               //Serial.println(ppm[2]);
-                    
+
+                                
+
+                             
                                 if (x >= LANDING_X_VALUE)
                                 {
-                                 Serial.println("Will roll to the left");
+                                 Serial.println("Will roll to the right");
                                 }
                                 else if (x <= (-1 * LANDING_X_VALUE) )
                                 {
-                                  Serial.println("Will roll to the right");
+                                  Serial.println("Will roll to the left");
                                 }
                                 else
                                 {
@@ -384,24 +384,24 @@ void loop() {
                               
                                 if (y >= LANDING_Y_VALUE)
                                   {
-                                     Serial.println("Will pitch to bottom");    
+                                     Serial.println("Will pitch to top");    
                                   }
                                 else if (y <= (-1 * LANDING_Y_VALUE) )
                                   {
-                                    Serial.println("Will pitch to up");
+                                    Serial.println("Will pitch to bottom");
                                   }
                                 else
                                   {
                                     Serial.println("Y is good");
                                   }
                     
-                               
+                             
                                    
                          }
                    
-            }
+               }
           
-  ppm[3] = CHANNEL_DEFAULT_VALUE;
+  //ppm[3] = CHANNEL_DEFAULT_VALUE;
   ppm[4] = ch5;
   ppm[5] = ch6;                
   }
